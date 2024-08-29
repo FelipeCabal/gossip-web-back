78771e2bf2cf82d3f39a21d287726c672546e873
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-08-29 02:41:45 -0500

    Conexion for database in  postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmConfig, mongooseConfigUri } from './config/data.source';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    MongooseModule.forRoot(mongooseConfigUri)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
