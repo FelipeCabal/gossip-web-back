8b96624f228d5f40a8bc1eeefbc2f033c2db3d00
ginaapaola <ginapaolamc07@gmail.com>
2024-11-30 00:16:43 -0500

    Servicios para eliminar mensajes

--- src/chats/services/mensajes.service.ts
+++ src/chats/services/mensajes.service.ts
@@ -1,15 +1,23 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Mensajes } from '../entities/mensajes.schema';
 import { UsersService } from 'src/users/services/users.service';
 import { CreateMessageDto } from '../dto/mensajesDto/create-mensaje.dto';
+import { PrivateChatsService } from './private-chats.service';
+import { Role } from 'src/config/enums/roles.enum';
+import { GroupChatsService } from './gruop-chats.service';
+import { ComunidadesService } from './comunity-chats.service';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class MessagesService {
     constructor(
         @InjectModel(Mensajes.name) private mensajeModel: Model<Mensajes>,
         private readonly usersService: UsersService, // Inyectamos el servicio de usuarios
+        private readonly privateChatsService: PrivateChatsService,
+        private readonly groupChatService: GroupChatsService,
+        private readonly comunidadesService: ComunidadesService
     ) { }
 
     // Crear un nuevo mensaje y asociarlo al usuario
@@ -56,5 +64,57 @@ export class MessagesService {
             createdAt,
             updatedAt,
         }));
+    }
+
+    async ClearChat(chatId: number, userId: number): Promise<{ deletedCount: number }> {
+
+        const chat = await this.privateChatsService.findOne(chatId);
+        const group = await this.groupChatService.findGroupById(chatId);
+        const comunidad = await this.comunidadesService.findCommunityById(chatId);
+
+        if (!chat && !group && !comunidad) {
+            throw new HttpException("Chat not found", HttpStatus.NOT_FOUND);
+        }
+
+        let chatType: string;
+        if (chat) chatType = 'private';
+        else if (group) chatType = 'group';
+        else if (comunidad) chatType = 'community';
+        else throw new HttpException("Chat not found", HttpStatus.NOT_FOUND);
+
+
+        if (chatType === 'private') {
+            if (chat.amistad.userEnvia.id !== userId && chat.amistad.userRecibe.id !== userId) {
+                throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+            }
+
+            const deletedMessages = await this.mensajeModel.deleteMany({ chatId });
+
+            return { deletedCount: deletedMessages.deletedCount };
+        }
+
+        if (chatType === 'group') {
+            const isMember = group.miembros.some((member) => member.id === userId);
+            if (!isMember) {
+                throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED);
+            }
+
+            const deletedMessages = await this.mensajeModel.deleteMany({ chatId });
+
+            return { deletedCount: deletedMessages.deletedCount }
+        }
+
+        if (chatType === 'community') {
+            const isOwner = comunidad.miembros.some((member) => member.rol === Role.Owner && member.id === userId);
+            const isAdmin = comunidad.miembros.some((member) => member.rol === Role.Admin && member.id === userId);
+
+            if (!isOwner && !isAdmin) {
+                throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+            }
+
+            const deletedMessages = await this.mensajeModel.deleteMany({ chatId });
+
+            return { deletedCount: deletedMessages.deletedCount }
+        }
     }
 }
